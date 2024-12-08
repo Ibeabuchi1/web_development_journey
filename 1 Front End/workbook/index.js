1a1dacf0767865a79b7c0b6f55cbe67d38e7d222
ibeabuchi1 <davidcibeabuchi@gmail.com>
2024-12-08 02:39:00 +0100

    Initial commit

--- 1 Front End/workbook/index.js
+++ 1 Front End/workbook/index.js
@@ -5,40 +5,69 @@ const template = document.querySelector("#animal-card");
 const container = document.createElement("div");
 console.log(pets);
 
-function checkAge(age) {
+function findAge(age) {
   if (!age) {
     return "Less than 1 year old";
   }
-  return age > 1 ? `${age} years old` : "1 year old";
+  return age > 1 ? `${age} years old` : " 1 year old";
 }
 
 pets.forEach((pet) => {
-  const clone = template.content.cloneNode(true);
+  var clone = template.content.cloneNode(true);
 
   clone.querySelector("h3").textContent = pet.name;
+  clone.querySelector(".description").textContent = pet.description;
+  clone.querySelector(".species").textContent = pet.species;
+  clone.querySelector(".primary-btn").innerHTML = `Adopt ${pet.name}`;
+  clone.querySelector(
+    ".primary-btn"
+  ).href = `https://learnwebcode.github.io/pet-adoption-data/pets/${pet.id}`;
 
   var img = clone.querySelector("img");
   img.src = `./images/${pet.name}.webp`;
-  img.alt = `a ${pet.species} named ${pet.name}`;
-
-  clone.querySelector(".description").textContent = pet.description;
+  img.alt = `A ${pet.species} named ${pet.name}.`;
 
   var age = new Date().getFullYear() - pet.birthYear;
-  var getAge = checkAge(age);
-  clone.querySelector(".age").textContent = getAge;
-  clone.querySelector(".species").textContent = pet.species;
-  clone.querySelector(".primary-btn").textContent = `Adopt ${pet.name}`;
-  clone.querySelector(
-    ".primary-btn"
-  ).href = `https://learnwebcode.github.io/pet-adoption-data/photos/${pet.id}.webp`;
+  var defineAge = findAge(age);
+  clone.querySelector(".age").textContent = defineAge;
 
   container.appendChild(clone);
 });
-document.querySelector(".animals-container").appendChild(container);
-// console.log(container);
 
-// git commands
-// git rm --cached -r -f . :: to remove all files from staging
-// git add . :: to add files to staging
-// git status :: to check staging area
-// git commit -m "Initial commit"
+document.querySelector(".animals-container").appendChild(container);
+
+function handleClickFilters(e) {
+  var target = e.target;
+
+  e.preventDefault();
+
+  filterButtons.forEach((el) => {
+    el.classList.remove("active");
+  });
+  target.classList.add("active");
+
+  filterSpecies(target.dataset.filter);
+}
+
+function filterSpecies(pet) {
+  var allPets = document.querySelectorAll(".animal-card");
+
+  if (pet == "all") {
+    allPets.forEach((e) => {
+      e.style.display = "";
+    });
+  } else {
+    allPets.forEach((e) => {
+      if (e.querySelector(".species").textContent == pet) {
+        e.style.display = "";
+      } else {
+        e.style.display = "none";
+      }
+    });
+  }
+}
+
+var filterButtons = document.querySelectorAll(".filter-nav a");
+filterButtons.forEach((el) => {
+  el.addEventListener("click", (e) => handleClickFilters(e));
+});
